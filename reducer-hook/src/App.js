eec686dfa253e8f9e4c4b307605b7a73c3972428
PratapTyagi <tyagipratap111@gmail.com>
2022-01-28 23:55:48 +0530

    Use reducer implementation

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,23 +1,46 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
+const initialState = {
+  user: 0,
+};
+
+const ACTIONS = {
+  INCREAMENT_USER: "increament",
+  DECREAMENT_USER: "decreament",
+  RESET: "reset",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREAMENT_USER:
+      return { ...state, user: state.user + action.payload };
+    case ACTIONS.DECREAMENT_USER:
+      return { ...state, user: state.user - action.payload };
+    case ACTIONS.RESET:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const [count, setCount] = useState(0);
-  const increament = (value) => {
-    setCount((prevValue) => prevValue + value);
-  };
-  const decreament = (value) => {
-    setCount((prevValue) => prevValue - value);
-  };
-  const reset = () => {
-    setCount(0);
-  };
+  const [userState, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="app">
-      <p>{`Count: ${count}`}</p>
-      <button onClick={() => increament(1)}>Increament</button>
-      <button onClick={() => decreament(1)}>Decreament</button>
-      <button onClick={() => reset()}>Reset</button>
+      <p>{`Count: ${userState.user}`}</p>
+      <button
+        onClick={() => dispatch({ type: ACTIONS.INCREAMENT_USER, payload: 5 })}
+      >
+        Increament
+      </button>
+      <button
+        onClick={() => dispatch({ type: ACTIONS.DECREAMENT_USER, payload: 1 })}
+      >
+        Decreament
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.RESET })}>Reset</button>
     </div>
   );
 };
